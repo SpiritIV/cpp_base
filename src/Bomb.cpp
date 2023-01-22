b815c3491820aeb1fce3b69963883720fd3fbc31
SpiritIV <ignatmatsal@mail.ru>
2023-01-22 22:24:24 +0300

    Homework for a lesson 1

---
+++ src/Bomb.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+#include "../include/Bomb.h"
+#include "../include/MyTools.h"
+
+using namespace std;
+using namespace MyTools;
+
+void Bomb::Draw() const
+{
+    MyTools::SetColor(CC_LightMagenta);
+    GotoXY(x, y);
+    cout << "*";
+}
